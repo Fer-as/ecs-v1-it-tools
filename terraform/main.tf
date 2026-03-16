7c35395c04852a7a69f52e0280643e74f6f7065e
Fer-as <ferasfaid.dev@gmail.com>
2026-03-16 06:59:44 +0000

    feat(terraform-root): wire custom vpc module into root configuration

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+locals {
+  app_domain = "${var.subdomain_name}.${var.domain_name}"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name         = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+}
