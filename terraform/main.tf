31331554592241fc28d5a9e629b9e13e99914c36
Fer-as <ferasfaid.dev@gmail.com>
2026-03-16 07:59:19 +0000

    feat(terraform-root): wire alb module into root configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,12 @@ module "ecr" {
 
   project_name = var.project_name
 }
+
+module "alb" {
+  source = "./modules/alb"
+
+  project_name      = var.project_name
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  certificate_arn   = var.certificate_arn
+}
