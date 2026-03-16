301e14189283a9cfd6db21b09598c0a855889883
Fer-as <ferasfaid.dev@gmail.com>
2026-03-16 06:11:55 +0000

    feat(terraform-vpc): add module variable definitions

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "AZs for subnets"
+  type        = list(string)
+}
